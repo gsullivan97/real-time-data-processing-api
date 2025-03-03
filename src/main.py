3674e0caf614aaeda416414f1d9bd2ed4bfd9232
gsullivan-ciandt <gregs@ciandt.com>
2025-03-03 13:18:35 -0300

    feat: Update dependencies and refactor FastAPI application lifespan management

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 #INFO: Main entry point for the FastAPI application
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.routers.data_router import router as data_router
 from src.app.data_generator import generate_data
+
 import asyncio
 
-app = FastAPI()
-
-app.include_router(data_router)
-
-@app.on_event("startup")
-async def startup_event():
-  print("Starting data generation")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
   asyncio.create_task(generate_data())
+
+  yield
+  print("Shutting down the application...")
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(data_router)
