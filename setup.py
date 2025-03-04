e71f9282235a8bd18b285906d9a75f213a349abe
gsullivan-ciandt <gregs@ciandt.com>
2025-03-04 08:22:52 -0300

    fix: Update setup.py to include new dependencies for FastAPI, Uvicorn, Pydantic, and Pandas

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ setup(
     packages=find_packages(),
     install_requires=[
         'pytest==7.1.2',
-        'virtualenv==20.29.2'
+        'virtualenv==20.29.2',
+        'fastapi==0.115.11',
+        'uvicorn==0.34.0',
+        'pydantic==2.10.6',
+        'pandas==2.2.3',
     ],
     entry_points={
         'console_scripts': [
-            'real-time-data-processing-api=real_time_data_processing_api.main:main',
+            'real-time-data-processing-api=src.main:main',
         ],
     },
 )
